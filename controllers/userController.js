46dd7f1b73503ce7f279d7882d3f07cf1597b375
mahmodam <91775238+mahmodam@users.noreply.github.com>
2023-04-16 22:21:02 +0300

    Reset password Url

--- controllers/userController.js
+++ controllers/userController.js
@@ -273,9 +273,7 @@ const forgotPassword = async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
 
     // Create reset password url
-    const resetUrl = `${req.protocol}://${req.get(
-      "host"
-    )}/api/password/reset/${resetToken}`;
+    const resetUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
 
     const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`;
 
