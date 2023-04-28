d4df37adc52049ba985dcf8a9637251cd75fcbfa
mahmodam <91775238+mahmodam@users.noreply.github.com>
2023-04-28 18:43:21 +0300

    update

--- app.js
+++ app.js
@@ -43,12 +43,12 @@ app.use("/api", order);
 
 // run backend and frontend on same port
 // after run npm run build in client folder
-if (process.env.NODE_ENV === "PRODUCTION") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "PRODUCTION") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+//   });
+// }
 
 // Middleware to handle errors
 app.use(errorMiddleware);
